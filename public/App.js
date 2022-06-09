af3f0622d130a85785052944fed7708065801c89
Yana Arkhipova <frontend.ark@gmail.com>
2022-06-09 13:39:38 +0600

    Update add_to_cart function

--- public/App.js
+++ public/App.js
@@ -2,7 +2,7 @@
   {
     var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
 
-    x = x * 1 + 1 // x = x + 1 умножение преобразует строку в цифру
+    x = x * 1 + 1; // x = x + 1 умножение преобразует строку в цифру
 
     window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 
@@ -11,5 +11,9 @@
 
   function add_to_cart(id)
   {
-    alert('You added pizza with id:' + id);
+    var key = 'product_' + id;
+
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
   }
